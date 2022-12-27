5e6ceaa5caa8a27082d2aba10da942a907c99f59
greytrick <patdg@pm.me>
2022-12-27 11:04:15 -0800

    Initial Commit

---
+++ AnimationSandbox/AnimationSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSandboxApp.swift
+//  AnimationSandbox
+//
+//  Created by pat on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
